05bfe86126f86aa9d9f09ce4235f381f8b642682
Avaz <futureprogrammer2021@gmail.com>
2024-12-03 10:31:56 +0500

    create soldier moving left and right

--- game1.py
+++ game1.py
@@ -20,3 +20,50 @@ moving_right = False
 
 #define colours
 BG = (144, 201, 120)
+
+def draw_bg():
+	screen.fill(BG)
+
+
+
+class Soldier(pygame.sprite.Sprite):
+	def __init__(self, char_type, x, y, scale, speed):
+		pygame.sprite.Sprite.__init__(self)
+		self.char_type = char_type
+		self.speed = speed
+		self.direction = 1
+		self.flip = False
+		img = pygame.image.load(f'img/{self.char_type}/Idle/0.png')
+		self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+		self.rect = self.image.get_rect()
+		self.rect.center = (x, y)
+
+
+	def move(self, moving_left, moving_right):
+		#reset movement variables
+		dx = 0
+		dy = 0
+
+		#assign movement variables if moving left or right
+		if moving_left:
+			dx = -self.speed
+			self.flip = True
+			self.direction = -1
+		if moving_right:
+			dx = self.speed
+			self.flip = False
+			self.direction = 1
+
+
+		#update rectangle position
+		self.rect.x += dx
+		self.rect.y += dy
+
+
+	def draw(self):
+		screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
+
+
+
+player = Soldier('player', 200, 200, 3, 5)
+enemy = Soldier('enemy', 400, 200, 3, 5)
