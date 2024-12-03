0bf4d4260e21a075dfd2c74fcbe13d605d89202a
Avaz <futureprogrammer2021@gmail.com>
2024-12-03 09:59:33 +0500

    create body soldier update

--- game.py
+++ game.py
@@ -12,7 +12,7 @@ pygame.display.set_caption('Shooter')
 class Soldier(pygame.sprite.Sprite):
     def __init__(self, x, y, scale):
         pygame.sprite.Sprite.__init__(self)
-		img = pygame.image.load('img/player/Idle/0.png')
+	    img = pygame.image.load('img/player/Idle/0.png')
 		self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
 		self.rect = self.image.get_rect()
 		self.rect.center = (x, y)
@@ -20,3 +20,5 @@ class Soldier(pygame.sprite.Sprite):
 	def draw(self):
 		screen.blit(self.image, self.rect)
   
+player = Soldier(200, 200, 3)
+player2 = Soldier(400, 200, 3)
