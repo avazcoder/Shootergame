305a69ffe1550ef7f83a289c3b9b00d3cad6008a
Avaz <futureprogrammer2021@gmail.com>
2024-12-03 10:33:26 +0500

    keyboard button released

--- game1.py
+++ game1.py
@@ -67,3 +67,43 @@ class Soldier(pygame.sprite.Sprite):
 
 player = Soldier('player', 200, 200, 3, 5)
 enemy = Soldier('enemy', 400, 200, 3, 5)
+
+run = True
+while run:
+
+	clock.tick(FPS)
+
+	draw_bg()
+
+	player.draw()
+	enemy.draw()
+
+	player.move(moving_left, moving_right)
+
+	for event in pygame.event.get():
+		#quit game
+		if event.type == pygame.QUIT:
+			run = False
+		#keyboard presses
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_a:
+				moving_left = True
+			if event.key == pygame.K_d:
+				moving_right = True
+			if event.key == pygame.K_ESCAPE:
+				run = False
+
+
+		#keyboard button released
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_a:
+				moving_left = False
+			if event.key == pygame.K_d:
+				moving_right = False
+
+
+
+
+	pygame.display.update()
+
+pygame.quit()
