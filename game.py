1b1af3944561df4be092e20b9d4d4e5b336c5a27
Avaz <futureprogrammer2021@gmail.com>
2024-12-03 09:57:59 +0500

    create class soldier update

--- game.py
+++ game.py
@@ -11,4 +11,12 @@ pygame.display.set_caption('Shooter')
 
 class Soldier(pygame.sprite.Sprite):
     def __init__(self, x, y, scale):
-        
+        pygame.sprite.Sprite.__init__(self)
+		img = pygame.image.load('img/player/Idle/0.png')
+		self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+		self.rect = self.image.get_rect()
+		self.rect.center = (x, y)
+
+	def draw(self):
+		screen.blit(self.image, self.rect)
+  
