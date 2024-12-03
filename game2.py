dc669374fd6c608bd02e378b562773abef33c050
Avaz <futureprogrammer2021@gmail.com>
2024-12-03 10:39:42 +0500

    update player actions

--- game2.py
+++ game2.py
@@ -133,3 +133,56 @@ class Soldier(pygame.sprite.Sprite):
 
 player = Soldier('player', 200, 200, 3, 5)
 enemy = Soldier('enemy', 400, 200, 3, 5)
+
+run = True
+while run:
+
+	clock.tick(FPS)
+
+	draw_bg()
+
+	player.update_animation()
+	player.draw()
+	enemy.draw()
+
+
+	#update player actions
+	if player.alive:
+		if player.in_air:
+			player.update_action(2)#2: jump
+		elif moving_left or moving_right:
+			player.update_action(1)#1: run
+		else:
+			player.update_action(0)#0: idle
+		player.move(moving_left, moving_right)
+
+
+	for event in pygame.event.get():
+		#quit game
+		if event.type == pygame.QUIT:
+			run = False
+		#keyboard presses
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_a:
+				moving_left = True
+			if event.key == pygame.K_d:
+				moving_right = True
+			if event.key == pygame.K_w and player.alive:
+				player.jump = True
+			if event.key == pygame.K_ESCAPE:
+				run = False
+
+
+		#keyboard button released
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_a:
+				moving_left = False
+			if event.key == pygame.K_d:
+				moving_right = False
+
+
+
+
+	pygame.display.update()
+
+pygame.quit()
