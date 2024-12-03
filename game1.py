d98ea34aabbadf1c563bad862bbabe01f409ce53
Avaz <futureprogrammer2021@gmail.com>
2024-12-03 10:29:54 +0500

    define player action variables

---
+++ game1.py
@@ -0,0 +1,22 @@
+import pygame
+
+pygame.init()
+
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Shooter')
+
+#set framerate
+clock = pygame.time.Clock()
+FPS = 60
+
+#define player action variables
+moving_left = False
+moving_right = False
+
+
+#define colours
+BG = (144, 201, 120)
