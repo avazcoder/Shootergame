e1675a1aea5a7b8351f76662913ac86925cecd02
Avaz <futureprogrammer2021@gmail.com>
2024-12-03 10:37:27 +0500

    update

---
+++ game2.py
@@ -0,0 +1,31 @@
+import pygame
+import os
+
+pygame.init()
+
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Shooter')
+
+#set framerate
+clock = pygame.time.Clock()
+FPS = 60
+
+#define game variables
+GRAVITY = 0.75
+
+#define player action variables
+moving_left = False
+moving_right = False
+
+
+#define colours
+BG = (144, 201, 120)
+RED = (255, 0, 0)
+
+def draw_bg():
+	screen.fill(BG)
+	pygame.draw.line(screen, RED, (0, 300), (SCREEN_WIDTH, 300))
