7e09b9cb8302b3f3620c77b1ca6ecad2f8f5e858
Avaz <futureprogrammer2021@gmail.com>
2024-12-05 14:04:34 +0500

    added colors

--- game.py
+++ game.py
@@ -5,9 +5,24 @@ pygame.init()
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
-
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Shooter')
+#set framerate
+clock = pygame.time.Clock()
+FPS = 60
+
+#define game variables
+GRAVITY = 0.75
+
+#define player action variables
+moving_left = False
+moving_right = False
+
+
+#define colours
+BG = (144, 201, 120)
+RED = (255, 0, 0)
+
 
 class Soldier(pygame.sprite.Sprite):
     def __init__(self, x, y, scale):
