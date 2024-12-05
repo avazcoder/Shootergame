e4b0e8b2522f0fd037b0923d0b11868265995640
Shukrullo <shukrulloqurbonov342@gmail.com>
2024-12-05 19:20:02 +0500

    added class Bullet

--- game.py
+++ game.py
@@ -155,7 +155,33 @@ class Soldier(pygame.sprite.Sprite):
 
 	def draw(self):
 		screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
-  
+
+
+class Bullet(pygame.sprite.Sprite):
+	def __init__(self, x, y, direction):
+		pygame.sprite.Sprite.__init__(self)
+		self.speed = 10
+		self.image = bullet_img
+		self.rect = self.image.get_rect()
+		self.rect.center = (x, y)
+		self.direction = direction
+
+	def update(self):
+		#move bullet
+		self.rect.x += (self.direction * self.speed)
+		#check if bullet has gone off screen
+		if self.rect.right < 0 or self.rect.left > SCREEN_WIDTH:
+			self.kill()
+
+		#check collision with characters
+		if pygame.sprite.spritecollide(player, bullet_group, False):
+			if player.alive:
+				player.health -= 5
+				self.kill()
+		if pygame.sprite.spritecollide(enemy, bullet_group, False):
+			if enemy.alive:
+				enemy.health -= 25
+				self.kill()
 player = Soldier('player',200, 200, 3, 5)
 enemy = Soldier('enemy',400, 200, 3, 5)
 
